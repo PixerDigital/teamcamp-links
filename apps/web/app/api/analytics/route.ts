d894b31601ddf08a0771b02040a70d700b2eb4f4
aniket-pixer <aniket@pixer.digital>
2025-03-18 17:41:43 +0530

    Fix changes in package.json

--- apps/web/app/api/analytics/route.ts
+++ apps/web/app/api/analytics/route.ts
@@ -23,13 +23,7 @@ export const GET = withWorkspace(
       analyticsPathParamsSchema.parse(params);
 
     // for backwards compatibility (we used to support /analytics/[endpoint] as well)
-    if (
-      !oldType &&
-      oldEvent &&
-      VALID_ANALYTICS_ENDPOINTS.includes(
-        oldEvent as (typeof VALID_ANALYTICS_ENDPOINTS)[number],
-      )
-    ) {
+    if (!oldType && oldEvent && VALID_ANALYTICS_ENDPOINTS.includes(oldEvent)) {
       oldType = oldEvent;
       oldEvent = undefined;
     }
